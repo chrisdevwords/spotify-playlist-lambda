11be565af68faeac318816367d294ada91f25b9c
chrisdevwords <chrisedwards@Chriss-MacBook-Pro.local>
2017-08-12 12:31:32 -0400

    Revert "verifying token is present. deploy test"
    This reverts commit 58bf3a514f5c3ea77062ebee8b4fc16ed10f81f8.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,11 @@ const { response } = require('./util/lambda')
 function handler(event, context, callback) {
 
     const { body } = event;
-    const {
-        SPOTIFY_USER_ACCESS_TOKEN,
-        SPOTIFY_LOCAL_URL,
-        SLACK_TOKEN
-    } = process.env;
+    const { SPOTIFY_CLIENT_ID, SPOTIFY_SECRET } = process.env;
 
-    let message = 'no token...';
-    if (SPOTIFY_USER_ACCESS_TOKEN) {
-        message = 'It works!'
-    }
     callback(null, response({
-        message,
-        body
+        message: 'It works!',
+        body: body
     }));
 }
 
